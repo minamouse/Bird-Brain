80ce48e208981e01f80e852019a8a8a8ec9c3f85
Marina Cottrell <marinacottrell@gmail.com>
2018-05-12 00:54:23 -0700

    Added new assets

---
+++ Assets/Scripts/Birb.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birb : MonoBehaviour {
+
+    Vector3 startPos;
+    Vector3 goalPos;
+    int time;
+    int currentTime;
+    bool isMoving;
+    Animator animations;
+    public BirbControl birbControl;
+
+	// Use this for initialization
+	void Start () {
+       // birbControl = GetComponent<BirbControl>();
+        animations = GetComponent<Animator>();
+        Debug.Log(birbControl);
+        startPos = birbControl.GetPosition();
+        transform.position = startPos;
+        time = 210;
+        currentTime = 0;
+        isMoving = false;
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (isMoving)
+        {
+            if (transform.position == goalPos) {
+                currentTime = 0;
+                isMoving = false;
+                animations.SetTrigger("stopFlying");
+                birbControl.ReturnPosition(startPos);
+                startPos = goalPos;
+            } else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, goalPos, Time.deltaTime * 10);
+            }
+        } else
+        {
+            currentTime += 1;
+            if (currentTime == time)
+            {
+                isMoving = true;
+                goalPos = birbControl.GetPosition();
+                transform.LookAt(goalPos);
+                animations.SetTrigger("startFlying");
+            }
+
+        }
+        
+		
+	}
+}
---
+++ Assets/Scripts/BirbControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirbControl : MonoBehaviour {
+
+    List<Vector3> allPositions;
+    // Use this for initialization
+    void Start () {
+        allPositions = new List<Vector3>
+        {
+            new Vector3(511.17f, 36.59f, 386.06f),
+            new Vector3(490.178f, 47.37f, 376.5f),
+            new Vector3(495.027f, 48.821f, 348.245f)
+        };
+    }
+
+    public Vector3 GetPosition ()
+    {
+        int ind = Random.Range(0,allPositions.Count-1);
+        Vector3 toReturn = allPositions[ind];
+        allPositions.RemoveAt(ind);
+        return toReturn;
+    }
+
+    public void ReturnPosition (Vector3 usedPos)
+    {
+        allPositions.Add(usedPos);
+    }
+}
